0aa97e20b725893e7ed99f27dbff9c70fbaf3779
Heraclitus <03m3333@gmail.com>
2021-10-08 07:24:57 -0700

    Adding droplet provissiong

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Running terraform requires...
 1. a file `./personal-access-token-2021` used for API access that terraform exploits.
 2. a file `./project_name.txt` used to reference the digital ocean project name.
 3. a file `./.my_ssh_ip.txt` used to establish ip-white list for droplet networking.
-4. running either `./runt.sh` *(short for run terraform)* or `./importt.sh` *(short for import terraform)*
+4. an ssh key file `$HOME/.ssh/id_rsa` used to make ssh connections.
+5. running either `./runt.sh` *(short for run terraform)* or `./importt.sh` *(short for import terraform)*
--- droplet.tf
+++ droplet.tf
@@ -1,28 +1,43 @@
 variable "region" {
  default = "SFO3"
 }
-# resource digitalocean_droplet "me" {
-#     name = "frontend"
-#     image = "ubuntu-20-04-x64"
-#     region = "SFO3"
-#     size = "s-1vcpu-1gb"
-#     ssh_keys = [
-#       data.digitalocean_ssh_key.lubuntu.id
-#     ]
-#     connection {
-#         host = self.ipv4_address
-#         user = "root"
-#         type = "ssh"
-#         private_key = file(var.pvt_key)
-#         timeout = "2m"
-#     }
 
-#     provisioner "remote-exec" {
-#         inline = [
-#             "export PATH=$PATH:/usr/bin",
-#             # install nginx
-#             "sudo apt update",
-#             "sudo apt install -y nginx"
-#         ]
-#     }
-# }
+locals {
+    instance_user = "${split(".", digitalocean_domain.mydomain.name)[0]}"
+}
+
+resource digitalocean_droplet "me" {
+    count = 0
+    name = "frontend-${count.index}"
+    image = "ubuntu-20-04-x64"
+    region = "SFO3"
+    size = "s-1vcpu-1gb"
+    ssh_keys = [
+      data.digitalocean_ssh_key.lubuntu.id
+    ]
+    connection {
+        host = self.ipv4_address
+        user = "root"
+        type = "ssh"
+        private_key = file(var.pvt_key)
+        timeout = "2m"
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            "export PATH=$PATH:/usr/bin",
+            "ufw allow OpenSSH",
+            "ufw --force enable",
+            "sudo apt update",
+            "sudo DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::=--force-confold -o Dpkg::Options::=--force-confdef -y --allow-downgrades --allow-remove-essential --allow-change-held-packages",
+            "sudo adduser --disabled-password --gecos \"\" ${local.instance_user}",
+            "usermod -aG sudo ${local.instance_user}",
+            "rsync --archive --chown=${local.instance_user}:${local.instance_user} ~/.ssh /home/${local.instance_user}",
+            # install nginx
+            "sudo apt install -y nginx",
+            "sudo ufw allow 'Nginx Full'",
+            "sudo mkdir -p /var/www/${digitalocean_domain.mydomain.name}/html",
+            "sudo apt install -y certbot python3-certbot-nginx"
+        ]
+    }
+}
